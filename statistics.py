886d9689951ad803768726c5f25cf14d0a6095ab
Aleksandr Semenov <semenlinux@gmail.com>
2021-02-28 20:44:39 +0300

    Calculate the average salary by language

--- statistics.py
+++ statistics.py
@@ -1,7 +1,8 @@
+import pprint
+from typing import Union
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
-from typing import Union
 
 
 def get_response_from_link(link: str, params: dict) -> requests.models.Response:
@@ -26,34 +27,50 @@ def predict_rub_salary(vacancy: dict) -> Union[int, None]:
     return None
 
 
+def predict_average_salary(vacancies: list) -> dict:
+    average_salary = 0
+    vacancies_processed = 0
+    for vacancy in vacancies:
+        salary_vacancy_average = predict_rub_salary(vacancy)
+        if salary_vacancy_average:
+            average_salary += salary_vacancy_average
+            vacancies_processed += 1
+    average_salary = average_salary / vacancies_processed
+    return {
+        "vacancies_processed": vacancies_processed,
+        "average_salary": int(average_salary),
+    }
+
+
 def main():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    number_vacancies = {}
+    salary_languages = {}
     hh_api_link = "https://api.hh.ru/vacancies"
     hh_params = {
         "area": 1,
         "period": 30,
     }
     programming_language_range = {
-        # "JavaScript": "программист JavaScript",
-        # "Java": "программист Java",
+        "JavaScript": "программист JavaScript",
+        "Java": "программист Java",
         "Python": "программист Python",
-        # "Ruby": "программист Ruby",
-        # "PHP": "программист PHP",
-        # "C++": "программист C++",
-        # "Swift": "программист Swift",
-        # "C#": "программист C#",
-        # "Scala": "программист Scala",
-        # "Go": "программист Go",
+        "Ruby": "программист Ruby",
+        "PHP": "программист PHP",
+        "C++": "программист C++",
+        "Swift": "программист Swift",
+        "C#": "программист C#",
+        "Scala": "программист Scala",
+        "Go": "программист Go",
     }
     for programming_language in programming_language_range:
         hh_params["text"] = programming_language_range.get(programming_language)
         hh_link_response = get_response_from_link(hh_api_link, hh_params)
-        number_vacancies[programming_language] = hh_link_response.json().get("found")
-        for vacancy_item in hh_link_response.json().get("items"):
-            print(vacancy_item.get("salary"))
-            print(predict_rub_salary(vacancy_item))
-    print(number_vacancies)
+        vacancy_items = hh_link_response.json().get("items")
+        salary_languages[programming_language] = predict_average_salary(vacancy_items)
+        salary_languages[programming_language][
+            "vacancies_found"
+        ] = hh_link_response.json().get("found")
+    pprint.pprint(salary_languages)
 
 
 if __name__ == "__main__":
