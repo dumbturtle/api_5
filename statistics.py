27cb0c2cd028f1dff118c5b4156254b03e1e5968
Aleksandr Semenov <semenlinux@gmail.com>
2021-03-08 15:52:06 +0300

    Dry the name of variables and functions

--- statistics.py
+++ statistics.py
@@ -46,7 +46,7 @@ def predict_rub_salary_for_superjob(vacancy: Dict) -> Optional[float]:
 
 
 def predict_average_salary_for_hh(api_link: str, request_params: Dict) -> Dict:
-    range_average_salaries = []
+    average_salaries = []
     page_content = {"pages": 100}
     for page in count():
         if page >= page_content["pages"]:
@@ -54,61 +54,57 @@ def predict_average_salary_for_hh(api_link: str, request_params: Dict) -> Dict:
         request_params["page"] = page
         page_response = get_response_from_link(api_link, params=request_params)
         page_content = page_response.json()
-        range_average_salaries += [
+        average_salaries += [
             predict_rub_salary_for_hh(vacancy) for vacancy in page_content.get("items")
         ]
-    average_salaries = calculate_average_salary_for_all_vacancies(
-        range_average_salaries
-    )
-    average_salaries["vacancies_found"] = page_content.get("found")
-    return average_salaries
+    hh_salaries = calculate_average_salary_for_all_vacancies(average_salaries)
+    hh_salaries["vacancies_found"] = page_content.get("found")
+    return hh_salaries
 
 
 def predict_average_salary_for_superjob(
     api_link: str, request_params: Dict, request_headers: Dict
 ) -> Dict:
-    range_average_salaries = []
+    average_salaries = []
     for page in count():
         request_params["page"] = page
         page_response = get_response_from_link(
             api_link, params=request_params, headers=request_headers
         )
         page_content = page_response.json()
-        range_vacancies = page_content.get("objects")
-        if not range_vacancies:
+        vacancies = page_content.get("objects")
+        if not vacancies:
             break
-        range_average_salaries += [
-            predict_rub_salary_for_superjob(vacancy) for vacancy in range_vacancies
+        average_salaries += [
+            predict_rub_salary_for_superjob(vacancy) for vacancy in vacancies
         ]
-    average_salaries = calculate_average_salary_for_all_vacancies(
-        range_average_salaries
-    )
-    average_salaries["vacancies_found"] = page_content.get("total")
-    return average_salaries
+    superjob_salaries = calculate_average_salary_for_all_vacancies(average_salaries)
+    superjob_salaries["vacancies_found"] = page_content.get("total")
+    return superjob_salaries
 
 
-def calculate_average_salary_for_all_vacancies(salary_range: List) -> Dict:
+def calculate_average_salary_for_all_vacancies(salaries: List) -> Dict:
     average_salary = 0
-    filtered_range_salaries: List[float] = list(filter(None, salary_range))
-    summation_result_salary = sum(filtered_range_salaries)
-    vacancies_processed = len(filtered_range_salaries)
+    filtered_salaries: List[float] = list(filter(None, salaries))
+    sum_salary = sum(filtered_salaries)
+    vacancies_processed = len(filtered_salaries)
     if not vacancies_processed == 0:
-        average_salary = int(summation_result_salary / vacancies_processed)
+        average_salary = int(sum_salary / vacancies_processed)
     return {
         "vacancies_processed": vacancies_processed,
         "average_salary": average_salary,
     }
 
 
-def collect_hh_all_statistic(programming_language_range: Dict, api_link: str) -> List:
+def collect_hh_statistics(programming_languages: Dict, api_link: str) -> List:
     hh_statistic = []
     hh_params = {
         "area": 1,
         "period": 30,
         "per_page": 20,
     }
-    for programming_language in programming_language_range:
-        hh_params["text"] = programming_language_range.get(programming_language)
+    for programming_language in programming_languages:
+        hh_params["text"] = programming_languages.get(programming_language)
         hh_language_salary = predict_average_salary_for_hh(api_link, hh_params)
         hh_statistic.append(
             [
@@ -121,9 +117,7 @@ def collect_hh_all_statistic(programming_language_range: Dict, api_link: str) ->
     return hh_statistic
 
 
-def collect_superjob_all_statistic(
-    programming_language_range: Dict, api_link: str
-) -> List:
+def collect_superjob_statistics(programming_languages: Dict, api_link: str) -> List:
     superjob_statistic = []
     superjob_headers = {
         "X-Api-App-Id": os.environ["SUPERJOB_API_KEY"],
@@ -131,10 +125,8 @@ def collect_superjob_all_statistic(
     superjob_params = {
         "town": 4,
     }
-    for programming_language in programming_language_range:
-        superjob_params["keyword"] = programming_language_range.get(
-            programming_language
-        )
+    for programming_language in programming_languages:
+        superjob_params["keyword"] = programming_languages.get(programming_language)
         superjob_language_salary = predict_average_salary_for_superjob(
             api_link, superjob_params, superjob_headers
         )
@@ -167,7 +159,7 @@ def main():
     superjob_api_link = os.environ["SUPERJOB_API_LINK"]
     hh_title = "HeadHunter Moscow"
     superjob_title = "SuperJob Moscow"
-    programming_language_range = {
+    programming_languages = {
         "JavaScript": "программист JavaScript",
         "Java": "программист Java",
         "Python": "программист Python",
@@ -180,14 +172,12 @@ def main():
         "Go": "программист Go",
     }
     try:
-        hh_all_statistic = collect_hh_all_statistic(
-            programming_language_range, hh_api_link
+        hh_statistics = collect_hh_statistics(programming_languages, hh_api_link)
+        superjob_statistics = collect_superjob_statistics(
+            programming_languages, superjob_api_link
         )
-        superjob_all_statistic = collect_superjob_all_statistic(
-            programming_language_range, superjob_api_link
-        )
-        hh_termial_table = create_table(hh_all_statistic, hh_title)
-        superjob_terminal_table = create_table(superjob_all_statistic, superjob_title)
+        hh_termial_table = create_table(hh_statistics, hh_title)
+        superjob_terminal_table = create_table(superjob_statistics, superjob_title)
         print(hh_termial_table)
         print()
         print(superjob_terminal_table)
